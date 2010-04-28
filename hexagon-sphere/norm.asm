1b6e71dfdba4a6a3a130788abd5d811ef325fead
Anja Berens <pharaun666@gmail.com>
2010-04-27 21:19:54 -0400

    got it to assembly and link properly! algo is still wrong

--- hexagon-sphere/norm.asm
+++ hexagon-sphere/norm.asm
@@ -1,13 +1,44 @@
-    global normalize	    ; (float v[3])
-    extern printf
-
-    section .text
-
+        .text
+.globl normalize
+        .type   normalize, @function
 normalize:
-    push    message
-    call    printf
-    add	    esp, 4
-    ret
-
-message:
-    db	'hello, world', 10, 0
+#NO_APP
+#	push   rbp
+        pushq   %rbp
+#	mov    rbp,rsp
+        movq    %rsp, %rbp
+#	mov    QWORD PTR [rbp-0x8],rdi
+        movq    %rdi, -8(%rbp)
+#	mov    rdx,QWORD PTR [rbp-0x8]
+        movq    -8(%rbp), %rdx
+#	mov    rax,QWORD PTR [rbp-0x8]
+        movq    -8(%rbp), %rax 
+#APP    
+#	movups xmm0,XMMWORD PTR [rax]
+        movups (%rax), %xmm0
+#	movaps xmm2,xmm0
+        movaps %xmm0, %xmm2 
+#	mulps  xmm0,xmm0
+        mulps %xmm0, %xmm0 
+#	movaps xmm1,xmm0
+        movaps %xmm0, %xmm1
+#	shufps xmm0,xmm1,0x4e
+        shufps $0x4e, %xmm1, %xmm0
+#	addps  xmm0,xmm1
+        addps %xmm1, %xmm0 
+#	movaps xmm1,xmm0
+        movaps %xmm0, %xmm1
+#	shufps xmm1,xmm1,0x11
+        shufps $0x11, %xmm1, %xmm1
+#	addps  xmm0,xmm1
+        addps %xmm1, %xmm0 
+#	rsqrtps xmm0,xmm0
+        rsqrtps %xmm0, %xmm0
+#	mulps  xmm2,xmm0
+        mulps %xmm0, %xmm2 
+#	movups XMMWORD PTR [rdx],xmm0
+        movups %xmm0, (%rdx)
+        
+#NO_APP
+	leave  
+	ret  
